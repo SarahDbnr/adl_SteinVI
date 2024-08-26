0591e0edf3c9ef54eeb96fd300872f3c6f80f047
n357797 <SDeubner@munichre.com>
2024-08-26 16:33:26 +0200

    dont calculate mse for multiclass

--- BNN_Example_clean_version/validation_and_evaluation.py
+++ BNN_Example_clean_version/validation_and_evaluation.py
@@ -1,14 +1,14 @@
 import jax
 import jax.numpy as jnp
 
-from BNN_Example_clean_version.plots.plot_mse import plot_mse
+# from plots.plot_mse import plot_mse
 
 
 # Evaluate the particles by averaging the predictions and calculating the accuracy
 def evaluate_particles(out, nnet_model, tree_def, x_input, true_output, model_regression):
     predictions, precisions = jax.vmap(lambda p: nnet_model.predict(tree_def(p), x_input))(out.particles)
-    mse = get_mse(predictions.squeeze(), true_output)
     if model_regression:
+        mse = get_mse(predictions.squeeze(), true_output)
         averaged_precision = precisions.squeeze().mean(0)
         print(f"Averaged precision: {averaged_precision}, mean squared error: {mse}")
         return mse, averaged_precision
@@ -16,8 +16,8 @@ def evaluate_particles(out, nnet_model, tree_def, x_input, true_output, model_re
         averaged_precision = precisions.squeeze().mean(0)
         predicted_classes = jnp.argmax(averaged_precision, axis=-1)
         accuracy = jnp.mean(predicted_classes == true_output)
-        print(f"Validation accuracy: {averaged_precision}, mean squared error: {mse}")
-        return mse, accuracy
+        print(f"Validation accuracy: {accuracy}")
+        return None, accuracy
 
 
 def evaluate_mse_on_test_data(particles, x_input, true_output, nnet_model, tree_def):
