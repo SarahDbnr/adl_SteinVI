df6e5dcb74c9b8592e58f5ded355b427e874da50
n357797 <SDeubner@munichre.com>
2024-08-26 17:38:23 +0200

    add different regression datasets and final fixes

--- BNN_Example_clean_version/data_handling.py
+++ BNN_Example_clean_version/data_handling.py
@@ -1,3 +1,6 @@
+import jax
+import jax.numpy as jnp
+
 VAL_SPLIT = 0.1
 FRACTION = 0.1
 
@@ -13,4 +16,50 @@ def apply_data_settings_keras(new_dataset, with_flattening):
     val_size = int(len(x_train) * VAL_SPLIT)
     x_val, y_val = x_train[-val_size:], y_train[-val_size:]
     x_train, y_train = x_train[:-val_size], y_train[:-val_size]
+
+    print_data_information(x_train, y_train, x_val, y_val, x_test, y_test)
+
     return x_train, y_train, x_val, y_val, x_test, y_test
+
+
+def apply_data_settings_sklearn(new_dataset):
+    key = jax.random.PRNGKey(1)
+    x = new_dataset.data
+    y = new_dataset.target
+    # Shuffle and split data
+    key, subkey = jax.random.split(key)
+    perm = jax.random.permutation(subkey, len(x))
+    x, y = x[perm], y[perm]
+
+    num_train = int(0.8 * len(x))
+    num_val = int(VAL_SPLIT * num_train)
+
+    x_train, y_train = x[:num_train], y[:num_train]
+    x_val, y_val = x_train[-num_val:], y_train[-num_val:]
+    x_train, y_train = x_train[:-num_val], y_train[:-num_val]
+    x_test, y_test = x[num_train:], y[num_train:]
+
+    print_data_information(x_train, y_train, x_val, y_val, x_test, y_test)
+
+    return x_train, y_train, x_val, y_val, x_test, y_test
+
+# TODO: if reduce_size: x_train, y_train, x_test, y_test = reduce_size_of_dataframe(FRACTION, x_train, x_test,
+#  y_train, y_test)
+
+
+def reduce_size_of_dataframe(fraction, x_train, x_test, y_train, y_test):
+    train_sample_size = int(x_train.shape[0] * fraction)
+    test_sample_size = int(x_test.shape[0] * fraction)
+    train_indices = jnp.random.choice(x_train.shape[0], train_sample_size, replace=False)
+    test_indices = jnp.random.choice(x_test.shape[0], test_sample_size, replace=False)
+    x_train = x_train[train_indices]
+    y_train = y_train[train_indices]
+    x_test = x_test[test_indices]
+    y_test = y_test[test_indices]
+    return x_train, y_train, x_test, y_test
+
+
+def print_data_information(x_train, y_train, x_val, y_val, x_test, y_test):
+    print(f"Training data shape: {x_train.shape}, Training labels shape: {y_train.shape}")
+    print(f"Validation data shape: {x_val.shape}, Validation labels shape: {y_val.shape}")
+    print(f"Test data shape: {x_test.shape}, Test labels shape: {y_test.shape}")
