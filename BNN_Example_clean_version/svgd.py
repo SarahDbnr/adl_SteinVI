f106c07efcbc46214091452ec3ab71f06162588c
Kilian Runnwerth <kilian.runnwerth@student.htw-berlin.de>
2024-08-29 11:31:03 +0200

    Changes so that MSE, Accuracy and val_precision can be plotted

--- BNN_Example_clean_version/svgd.py
+++ BNN_Example_clean_version/svgd.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 import jax
 import jax.numpy as jnp
 
-from validation_and_evaluation import get_mse_and_accuracy_over_predictions
+from validation_and_evaluation import get_evaluation_metrics_over_predictions
 from BNN_Model import build_model
 from get_posteriori import get_posteriori
 
@@ -39,7 +39,7 @@ def train_with_svgd(dataset, output_size, network_structure, batch_size, num_par
     logp_model = get_posteriori(nnet_model, tree_def, regression, pen_lambda)
 
     # Run SVGD training loop with Adam optimizer and validation accuracy tracking
-    out, mse, val_accuracies = svgd_training_loop(
+    out, evaluation_metrics_1, evaluation_metrics_2 = svgd_training_loop(
         log_p=logp_model,
         initial_position=initial_particles_vector,
         initial_kernel_parameters={"length_scale": KERNEL_LENGTH},
@@ -58,7 +58,16 @@ def train_with_svgd(dataset, output_size, network_structure, batch_size, num_par
 
     # TODO: plot mse, val_accuracies
 
-    return out, z_test, y_test, nnet_model, tree_def
+    return out, z_test, y_test, nnet_model, tree_def, evaluation_metrics_1,evaluation_metrics_2
+
+
+#  Initialize particles for the SVGD algorithm
+def initialize_particles(param_vec, rng_key_init, num_particles):
+    initial_particles_vector = jax.random.normal(
+        rng_key_init,
+        shape=(num_particles,) + param_vec.shape
+    )
+    return initial_particles_vector
 
 
 # SVGD training loop with early stopping
@@ -83,11 +92,11 @@ def svgd_training_loop(
     state = svgd.init(initial_position, initial_kernel_parameters)
     step = jax.jit(svgd.step)
 
-    best_val_accuracy = float('-inf')
+    best_evaluation_metrics_1 = float('-inf')
     patience_counter = 0
     best_state = None
-    mse = []
-    val_accuracies = []
+    evaluation_metrics_1 = [] #mse and accuracy
+    evaluation_metrics_2 = [] #val_accuracies
 
     # Define a training step function that JIT compiles the SVGD step
     @jax.jit
@@ -104,12 +113,11 @@ def svgd_training_loop(
             state = training_step(state, z_train, y_train)
 
         # TODO: Check time effort for mse and accuracy calc and use as option only
-        current_mse, val_accuracy = get_mse_and_accuracy_over_predictions(state, nnet_model, tree_def, z_val, y_val,
-                                                                          regression)
-        val_accuracies.append(val_accuracy)
-        mse.append(current_mse)
-
-        best_state, best_val_accuracy, patience_counter = check_for_early_stopping(val_accuracy, best_val_accuracy,
+        current_evaluation_metrics_1, current_evaluation_metrics_2 = get_evaluation_metrics_over_predictions(state, nnet_model, tree_def, z_val, y_val, regression)
+        evaluation_metrics_2.append(current_evaluation_metrics_2)
+        evaluation_metrics_1.append(current_evaluation_metrics_1)
+        print(f"\nEval_Metric: {current_evaluation_metrics_1}")
+        best_state, best_evaluation_metrics_1, patience_counter = check_for_early_stopping(current_evaluation_metrics_1, best_evaluation_metrics_1,
                                                                                    iteration, state, best_state,
                                                                                    patience_counter)
         if patience_counter >= PATIENCE:
@@ -120,40 +128,10 @@ def svgd_training_loop(
     if best_state is None:
         best_state = state
 
-    return best_state, mse, val_accuracies
+    return best_state, evaluation_metrics_1, evaluation_metrics_2
 
 
-def initialize_particles(param_vec, rng_key_init, num_particles):
-    initial_particles_vector = jax.random.normal(
-        rng_key_init,
-        shape=(num_particles,) + param_vec.shape
-    )
-    return initial_particles_vector
-
-def create_minibatches(batch_size, input_data, output_data, key):
-    if batch_size != 0:
-        if batch_size is None:
-            num_batches = DEFAULT_NUM_BATCHES
-        elif len(input_data) < batch_size:
-            num_batches = DEFAULT_NUM_BATCHES
-            print("\n WARNING: Batch size to large default batch size will be used!")
-        else:
-            num_batches = len(input_data) // batch_size
-        input_data, output_data = shuffle_paired_data(key, input_data, output_data)
-        input_data = jnp.array_split(input_data, num_batches)
-        output_data = jnp.array_split(output_data, num_batches)
-        return input_data, output_data
-    return input_data, output_data
-
-@jax.jit
-def shuffle_paired_data(key, input_data, output_data):
-    num_samples = input_data.shape[0]
-    permutation = jax.random.permutation(key, num_samples)
-    shuffled_input = jnp.take(input_data, permutation, axis=0)
-    shuffled_output = jnp.take(output_data, permutation, axis=0)
-    return shuffled_input, shuffled_output
-
-def get_adam_optimizer():  # TODO: exponential decay nur als option default const or less drastic decay then exponential
+def get_adam_optimizer(): # TODO: exponential decay nur als option default const or less drastic decay then exponential
     # stepwise decay check how high decay rate should be
     learning_rate_schedule = exponential_decay(
         init_value=INITIAL_LEARNING_RATE,
@@ -167,14 +145,15 @@ def get_adam_optimizer():  # TODO: exponential decay nur als option default cons
 
     return adam(learning_rate_schedule)
 
-def check_for_early_stopping(val_accuracy, best_val_accuracy, iteration, state, best_state, patience_counter):
+
+def check_for_early_stopping(val_accuracy, best_evaluation_metrics_1, iteration, state, best_state, patience_counter):
     # Apply early stopping logic only after warm-up period
     if iteration >= WARM_UP_ITERATIONS:
-        if val_accuracy > best_val_accuracy + MIN_DELTA:
-            best_val_accuracy = val_accuracy
+        if val_accuracy > best_evaluation_metrics_1 + MIN_DELTA:
+            best_evaluation_metrics_1 = val_accuracy
             patience_counter = 0
             best_state = state
         else:
             patience_counter += 1
             return None, float('-inf'), patience_counter
-    return best_state, best_val_accuracy, patience_counter
+    return best_state, best_evaluation_metrics_1, patience_counter
