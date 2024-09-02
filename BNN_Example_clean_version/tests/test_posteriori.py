e31bbcd0f4bb080e400bec65c0f47c3bdf1e6f58
Sarah Deubner <SDeubner@munichre.com>
2024-09-02 15:12:58 +0200

    add tests

---
+++ BNN_Example_clean_version/tests/test_posteriori.py
@@ -0,0 +1,31 @@
+import jax
+import jax.numpy as jnp
+
+from BNN_Example_clean_version.get_posteriori import logp_unnormalized_posterior_regression, link_function
+from BNN_Example_clean_version.regression_toy_example import get_regression_toy_example
+from BNN_Example_clean_version.BNN_Model import build_model
+
+
+def test_logp_unnormalized_posterior_regression():
+    # given
+    key = jax.random.PRNGKey(1)
+    z_train, y_train, z_val, y_val, z_test, y_test = get_regression_toy_example(num_points=10, input_dimension=2)
+    nnet_model, tree_def, param_vec = build_model(key, z_train, output_size=2,
+                                                  hidden_layers=(200, 75, 40),
+                                                  use_for_regression=True)
+    pen_lambda = 0
+    # when
+    likelihood = logp_unnormalized_posterior_regression(param_vec, z_train, y_train, nnet_model, tree_def, pen_lambda)
+    print(likelihood)
+
+
+def test_link_function_in_vmap():
+    # given
+    max_scale = 5
+    prediction = jnp.arange(10)
+    # when
+    scale = jax.vmap(lambda p: link_function(p, max_scale))(prediction)
+    # then
+    assert scale.shape == prediction.shape
+    assert scale[0] == jnp.log(2)
+    assert scale[scale > max_scale].shape == (0,)
