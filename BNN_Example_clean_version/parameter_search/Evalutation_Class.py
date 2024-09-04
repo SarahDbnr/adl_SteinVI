dc9019bfec5d36a0137e299287f77080f0dd11a2
n357797 <SDeubner@munichre.com>
2024-09-04 11:53:11 +0200

    Calulate EvaluationRegression variables

--- BNN_Example_clean_version/parameter_search/Evalutation_Class.py
+++ BNN_Example_clean_version/parameter_search/Evalutation_Class.py
@@ -1,14 +1,28 @@
+from BNN_Example_clean_version.Parameter_Class import Parameter
+
+
 class EvaluationRegression:
     name: str
     parameter_setting: Parameter
     mean_true_output: float
     var_true_output: float
     mean_prediction: float
-    var_prediction: float
+    average_var_prediction: float
     particle_span_predictions: float
     mean_precision: float
     var_precision: float
     mse: float
+
+    def __init__(self, name, true_output, test_predictions, test_precision):
+        self.name = name
+        mean_true_output = true_output.mean()
+        var_true_output = true_output.var()
+        mean_prediction = test_predictions.mean()
+        average_var_prediction = test_predictions.var(0).mean()
+        # TODO: particle_span_predictions
+        mean_precision = test_precision.mean()
+        var_precision = test_precision.var()
+        # TODO: mse
 
 
 class EvaluationMulticlass:
