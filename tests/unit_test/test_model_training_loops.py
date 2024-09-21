9a9bb9330d394745b4a3527f1a587c561a02d087
n357797 <SDeubner@munichre.com>
2024-09-21 16:20:38 +0200

    final test_model_training_loops.py

--- tests/unit_test/test_model_training_loops.py
+++ tests/unit_test/test_model_training_loops.py
@@ -1,5 +1,7 @@
 import pytest
 import jax
+import jax.numpy as jnp
+import copy
 
 from fixtures import stein_vi_regression_example, get_regression_toy_example
 
@@ -10,8 +12,6 @@ from stein_vi.algorithm.model_training import (no_minibatch_training_loop, data_
                                                data_and_particle_minibatch_training_loop,
                                                create_particle_minibatch_indices,
                                                create_minibatches)
-
-# TODO: this whole file for multiclass
 
 
 @pytest.mark.parametrize(
@@ -30,10 +30,11 @@ def test_no_minibatch_training_loop(stein_vi_regression_example, get_regression_
     stein_vi_regression_example.parameter.num_iterations = 2
     stein_vi_regression_example.parameter.patience_early_stopping = patience_early_stopping
     stein_vi_regression_example._full_evaluation = True
-    stein_vi_regression_example = set_up_svgd(stein_vi_regression_example)
+    set_up_svgd(stein_vi_regression_example)
+    stein_vi_regression_example_copy = copy.copy(stein_vi_regression_example)
 
     # when
-    trained_stein_vi = no_minibatch_training_loop(stein_vi_regression_example, regression_toy_example)
+    no_minibatch_training_loop(stein_vi_regression_example_copy, regression_toy_example)
 
     # then
     stein_vi_regression_example.state = stein_vi_regression_example.update_fn(stein_vi_regression_example.state,
@@ -41,7 +42,10 @@ def test_no_minibatch_training_loop(stein_vi_regression_example, get_regression_
     if patience_early_stopping > 1:
         stein_vi_regression_example.state = stein_vi_regression_example.update_fn(stein_vi_regression_example.state,
                                                                                   z_train, y_train)
-    assert stein_vi_regression_example == trained_stein_vi
+    assert jnp.all(jnp.array(stein_vi_regression_example.evaluation_metrics_1) ==
+                   jnp.array(stein_vi_regression_example_copy.evaluation_metrics_1))
+    assert jnp.all(jnp.array(stein_vi_regression_example.evaluation_metrics_2) ==
+                   jnp.array(stein_vi_regression_example_copy.evaluation_metrics_2))
 
 
 @pytest.mark.parametrize(
@@ -61,12 +65,13 @@ def test_data_minibatch_training_loop(stein_vi_regression_example, get_regressio
     stein_vi_regression_example.parameter.num_iterations = 2
     stein_vi_regression_example.parameter.patience_early_stopping = patience_early_stopping
     stein_vi_regression_example._full_evaluation = True
-    stein_vi_regression_example = set_up_svgd(stein_vi_regression_example)
+    set_up_svgd(stein_vi_regression_example)
+    stein_vi_regression_example_copy = copy.copy(stein_vi_regression_example)
 
     z_train_batched, y_train_batched = create_minibatches(36, z_train, y_train, key)
 
     # when
-    trained_stein_vi = data_minibatch_training_loop(stein_vi_regression_example, regression_toy_example, key)
+    data_minibatch_training_loop(stein_vi_regression_example_copy, regression_toy_example, key)
 
     # then
     stein_vi_regression_example.state = stein_vi_regression_example.update_fn(stein_vi_regression_example.state,
@@ -80,7 +85,11 @@ def test_data_minibatch_training_loop(stein_vi_regression_example, get_regressio
         stein_vi_regression_example.state = stein_vi_regression_example.update_fn(stein_vi_regression_example.state,
                                                                                   z_train_batched[1],
                                                                                   y_train_batched[1])
-    assert stein_vi_regression_example == trained_stein_vi
+
+    assert jnp.all(jnp.array(stein_vi_regression_example.evaluation_metrics_1) ==
+                   jnp.array(stein_vi_regression_example_copy.evaluation_metrics_1))
+    assert jnp.all(jnp.array(stein_vi_regression_example.evaluation_metrics_2) ==
+                   jnp.array(stein_vi_regression_example_copy.evaluation_metrics_2))
 
 
 @pytest.mark.parametrize(
@@ -101,14 +110,15 @@ def test_particle_minibatch_training_loop(stein_vi_regression_example, get_regre
     stein_vi_regression_example.parameter.num_iterations = 2
     stein_vi_regression_example.parameter.patience_early_stopping = patience_early_stopping
     stein_vi_regression_example._full_evaluation = True
-    stein_vi_regression_example = set_up_svgd(stein_vi_regression_example)
+    set_up_svgd(stein_vi_regression_example)
+    stein_vi_regression_example_copy = copy.copy(stein_vi_regression_example)
 
     particle_indices_batches = create_particle_minibatch_indices(key,
                                                                  stein_vi_regression_example.state.particles.shape[0],
                                                                  batch_size=5)
 
     # when
-    trained_stein_vi = particle_minibatch_training_loop(stein_vi_regression_example, regression_toy_example, key)
+    particle_minibatch_training_loop(stein_vi_regression_example_copy, regression_toy_example, key)
 
     # then
     stein_vi_regression_example.state = stein_vi_regression_example.update_fn(stein_vi_regression_example.state,
@@ -124,7 +134,11 @@ def test_particle_minibatch_training_loop(stein_vi_regression_example, get_regre
         stein_vi_regression_example.state = stein_vi_regression_example.update_fn(stein_vi_regression_example.state,
                                                                                   z_train, y_train,
                                                                                   particle_indices=particle_indices_batches[1])
-    assert stein_vi_regression_example == trained_stein_vi
+
+    assert jnp.all(jnp.array(stein_vi_regression_example.evaluation_metrics_1) ==
+                   jnp.array(stein_vi_regression_example_copy.evaluation_metrics_1))
+    assert jnp.all(jnp.array(stein_vi_regression_example.evaluation_metrics_2) ==
+                   jnp.array(stein_vi_regression_example_copy.evaluation_metrics_2))
 
 
 @pytest.mark.parametrize(
@@ -145,7 +159,8 @@ def test_data_and_particle_minibatch_training_loop(stein_vi_regression_example, 
     stein_vi_regression_example.parameter.num_iterations = 2
     stein_vi_regression_example.parameter.patience_early_stopping = patience_early_stopping
     stein_vi_regression_example._full_evaluation = True
-    stein_vi_regression_example = set_up_svgd(stein_vi_regression_example)
+    set_up_svgd(stein_vi_regression_example)
+    stein_vi_regression_example_copy = copy.copy(stein_vi_regression_example)
 
     particle_indices_batches = create_particle_minibatch_indices(key,
                                                                  stein_vi_regression_example.state.particles.shape[0],
@@ -153,8 +168,7 @@ def test_data_and_particle_minibatch_training_loop(stein_vi_regression_example, 
     z_train_batched, y_train_batched = create_minibatches(36, z_train, y_train, key)
 
     # when
-    trained_stein_vi = data_and_particle_minibatch_training_loop(stein_vi_regression_example, regression_toy_example,
-                                                                 key)
+    data_and_particle_minibatch_training_loop(stein_vi_regression_example_copy, regression_toy_example, key)
 
     # then
     stein_vi_regression_example.state = stein_vi_regression_example.update_fn(stein_vi_regression_example.state,
@@ -190,4 +204,8 @@ def test_data_and_particle_minibatch_training_loop(stein_vi_regression_example, 
                                                                                   y_train_batched[1],
                                                                                   particle_indices=
                                                                                   particle_indices_batches[1])
-    assert stein_vi_regression_example == trained_stein_vi
+
+    assert jnp.all(jnp.array(stein_vi_regression_example.evaluation_metrics_1) ==
+                   jnp.array(stein_vi_regression_example_copy.evaluation_metrics_1))
+    assert jnp.all(jnp.array(stein_vi_regression_example.evaluation_metrics_2) ==
+                   jnp.array(stein_vi_regression_example_copy.evaluation_metrics_2))
