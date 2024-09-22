8c685b57a8105a0c16dd93a57762facfbe00c80f
n357797 <SDeubner@munichre.com>
2024-09-22 20:50:56 +0200

    format BNN_Model.py

--- run_stein_vi/model/BNN_Model.py
+++ run_stein_vi/model/BNN_Model.py
@@ -1,17 +1,18 @@
 import flax.linen as nn
 from typing import Sequence
 
+
 class FlexibleSimpleNN(nn.Module):
     """
     A flexible neural network model that can be customized for different architectures and tasks.
 
     Attributes:
-        hidden_layers (Sequence[int]): Sequence of integers where each integer defines the number of neurons in a hidden layer. :no-index:
+        hidden_layers (Sequence[int]): Sequence of integers where each integer defines the number of neurons in a
+        hidden layer. :no-index:
         output_size (int): Size of the output layer. :no-index:
         activation (callable): Activation function to use in the hidden layers. :no-index:
         kernel_init (callable): Initialization function for kernel weights. :no-index:
         bias_init (callable): Initialization function for biases. :no-index:
-        use_for_regression (bool): Flag to determine whether the network is used for regression or classification. :no-index:
     """
     hidden_layers: Sequence[int] = (50,)
     output_size: int = 1
@@ -47,14 +48,11 @@ def build_model(hidden_layers=(50,), output_size=10, activation=nn.relu,
     Builds and initializes a neural network model based on specified configurations.
 
     Args:
-        key: JAX random key for initialization.
-        x_train (jax.numpy.ndarray): Sample input data used to define input shape.
         hidden_layers (tuple, optional): Tuple defining the number of units in each hidden layer.
         output_size (int, optional): The size of the output layer.
         activation (callable, optional): Activation function for the hidden layers.
         kernel_init (callable, optional): Weight initialization function.
         bias_init (callable, optional): Bias initialization function.
-        use_for_regression (bool, optional): Flag to specify if the model is intended for regression.
 
     Returns:
         tuple: The initialized model, the tree definition for parameter transformation, and a flattened parameter vector.
